bd45914e2fbe6fe8f1f2bf1bc4bee6b3bd5f0b51
Andreas Ottosson <andreasottosson@me.com>
2020-04-13 16:20:05 +0200

    - Added WiFi & IFTTT notifications

--- mousetrap_monitoring_arduino.ino
+++ mousetrap_monitoring_arduino.ino
@@ -1,7 +1,16 @@
 // TODO:
-// - Add WiFi
-// - Add IFTTT notifications
-// - (Add MQTT support)
+// - Future: Add MQTT support
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+#ifndef STASSID
+#define STASSID ""
+#define STAPSK  ""
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
 
 struct TRAP
 {
@@ -16,9 +25,44 @@ struct TRAP trapPins[] = {
     {4, "trap2", false}
 };
 
+void sendNotification(String trap) {
+
+    if ((WiFi.status() == WL_CONNECTED)) {
+
+        WiFiClient client;
+        HTTPClient http;
+
+        Serial.println("[HTTP] begin..");
+
+        http.begin(client, "http://maker.ifttt.com/trigger/your_ifttt_event/with/key/your_ifttt_key");
+        http.addHeader("Content-Type", "application/json");
+
+        Serial.print("[HTTP] POST...");
+
+        int httpCode = http.POST("{\"value1\":\""+trap+"\"}");
+
+        if (httpCode > 0) {
+        Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+
+        if (httpCode == HTTP_CODE_OK) {
+            const String& payload = http.getString();
+            Serial.println("received payload:\n<<");
+            Serial.println(payload);
+            Serial.println(">>");
+        }
+        } else {
+            Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        }
+
+        http.end();
+    }
+
+}
+
 void trapEventHandler(struct TRAP *trap) {
     if (trap->triggered == true) {
         Serial.println("Trap triggered: "+trap->name);
+        sendNotification(trap->name);
     } else if (trap->triggered == false) {
         Serial.println("Trap reset: "+trap->name);
     }
@@ -26,6 +70,20 @@ void trapEventHandler(struct TRAP *trap) {
 
 void setup() {
     Serial.begin(9600);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
     for (byte i = 0; i < (sizeof(trapPins) / sizeof(trapPins[0])); i++ ) {
         pinMode(trapPins[i].pin, INPUT);
     }
@@ -33,6 +91,7 @@ void setup() {
 
 void loop() {
     int trapCurrentValue;
+
     for (byte i = 0; i < (sizeof(trapPins) / sizeof(trapPins[0])); i++ ) {
         trapCurrentValue = trapPins[i].triggered;
         trapPins[i].triggered = digitalRead(trapPins[i].pin);
